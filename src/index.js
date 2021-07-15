5de7c466745384706e212967399c2ad0a8038850
Lina <31240383+linabiel@users.noreply.github.com>
2021-07-15 17:27:06 +0100

    added title

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Homepage />
   </React.StrictMode>,
   document.getElementById('root')
 );
