8b99bdbe5c90a6e432fb5f8ec1f16851cbc776d8
Lina <31240383+linabiel@users.noreply.github.com>
2021-07-15 17:26:25 +0100

    added title

---
+++ src/components/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Homepage() {
+
+    return(
+        <>
+        <div id="title">
+            <h1>Lina's Portfolio</h1>
+        </div>
+        </>
+    )
+}
+
+export default Homepage;
