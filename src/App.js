7aa6c5bd6a868cec90e1391a8d87e7d997ca44d4
Lina <31240383+linabiel@users.noreply.github.com>
2021-07-19 15:02:25 +0100

    replaced hashrouter with router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import MainContainer from './containers/Main';
 import Portfolio from './containers/Portfolio';
 
 function App() {
--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -1,5 +1,12 @@
-import React, {Component} from 'react';
-import {Route, NavLink, HashRouter} from "react-router-dom";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 import Home from '../components/Home';
 import About from '../components/About';
@@ -16,30 +23,32 @@ class Portfolio extends Component {
 
         return (
 
+            <>
 
-            <HashRouter>
+                <Router>
 
-                <div className='nav-bar'>
-                    <li className='nav-bar-item'><NavLink to="/">Home</NavLink></li>
-                    <li className='nav-bar-item'><NavLink to="/about">About</NavLink></li>
-                    <li className='nav-bar-item'><NavLink to="/projects">Projects</NavLink></li>
-                </div>
-
-
-                <div>
-                    <Route id='main-home' path="" component={Home} />
-
-                    <Route id='main-about' path="/about" component={About} />
-
-                    <Route id='main-projects' path="/projects" component={Projects} />
-                </div>
+                    <div className='nav-bar'>
+                        <li className='nav-bar-item'><Link to="/">Home</Link></li>
+                        <li className='nav-bar-item'><Link to="/about">About</Link></li>
+                        <li className='nav-bar-item'><Link to="/projects">Projects</Link></li>
+                    </div>
 
 
+                    <div>
+                    <Switch>
+                        <Route id='main-home' exact path="/" component={Home} />
+                        <Route id='main-about' exact path="/about" component={About} />
+                        <Route id='main-projects' exact path="/projects" component={Projects} />
+                    </Switch>
+                    </div>
+
+                </Router>
 
 
                 <footer>&copy; 2021 Lina Biel</footer>
 
-            </HashRouter>
+
+            </>
 
         )
     }
