c9e53c52c86a05bf2a40d321f929f9807dc59ba1
Lina <31240383+linabiel@users.noreply.github.com>
2021-07-19 21:46:00 +0100

    css on about me

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ class About extends Component {
 
         return (
 
-            <div id='main-about'>
-                <h2>
+            <div className='main-about'>
+                <h2 className='main-about'>
                     I am an inquisitive Software Developer, on a constant mission to expand my
                     knowledge. Throughout my career in the Art and Culture sector, I have developed
                     a strong analytical and critical thinking mindset. Having worked for over 10 years in
@@ -17,11 +17,11 @@ class About extends Component {
                     my existing skills and bring in the creativity I nourished as a freelance art critic.
                 </h2>
 
-                <h2>
+                <h2 id='cc-grad'>
                     CodeClan Graduate
-                <h2>
-
                 </h2>
+
+                <h2 className='main-about'>
                     A 16 week Professional Software Development Course that is accredited by the SQA. 800+ hours of learning in-demand methodology, languages, databases and frameworks. Solo and Group Projects, pair-programming, coding and presenting working apps in under a week.
                 </h2>
             </div>
