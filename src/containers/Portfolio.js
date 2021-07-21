828c1c76cf09823975fb11c72ecdc1a0dddfb921
Lina <31240383+linabiel@users.noreply.github.com>
2021-07-21 15:17:24 +0100

    added linkedin and github links

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
 
 
 import Home from '../components/Home';
@@ -24,10 +24,19 @@ class Portfolio extends Component {
                 <Router>
 
                     <div className='nav-bar'>
-                        <li className='nav-bar-item'><Link className='nav-bar-item' to="/portfolio">Home</Link></li>
-                        <li className='nav-bar-item'><Link className='nav-bar-item' to="/about">About</Link></li>
-                        <li className='nav-bar-item'><Link className='nav-bar-item' to="/projects">Projects</Link></li>
+                        <div className='nav-bar'>
+                            <li className='nav-bar-link'><Link className='nav-bar-link' to={{ pathname: "https://www.linkedin.com/in/lina-biel" }} target="_blank" >LinkedIn</Link></li>
+                            <li className='nav-bar-link'><Link className='nav-bar-link' to={{ pathname: "https://github.com/linabiel" }} target="_blank" >GitHub</Link></li>
+                        </div>
+
+                        <div className='nav-bar'>
+                            <li className='nav-bar-item'><Link className='nav-bar-item' to="/portfolio">Home</Link></li>
+                            <li className='nav-bar-item'><Link className='nav-bar-item' to="/about">About</Link></li>
+                            <li className='nav-bar-item'><Link className='nav-bar-item' to="/projects">Projects</Link></li>
+                        </div>
+
                     </div>
+
 
 
                     <div>
