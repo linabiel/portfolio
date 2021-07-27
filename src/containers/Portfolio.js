44ee36d0a7fc92c2838057b05d639cef6f51752d
Lina <31240383+linabiel@users.noreply.github.com>
2021-07-27 14:32:17 +0100

    trying to solve 4o4 error when i refresh the gh-page by adding back  /portfolio to each root

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -34,8 +34,8 @@ class Portfolio extends Component {
 
                         <div className='nav-bar'>
                             <li className='nav-bar-item'><Link className='nav-bar-item' to="/portfolio">Home</Link></li>
-                            <li className='nav-bar-item'><Link className='nav-bar-item' to="/about">About</Link></li>
-                            <li className='nav-bar-item'><Link className='nav-bar-item' to="/projects">Projects</Link></li>
+                            <li className='nav-bar-item'><Link className='nav-bar-item' to="/portfolio/about">About</Link></li>
+                            <li className='nav-bar-item'><Link className='nav-bar-item' to="/portfolio/projects">Projects</Link></li>
                         </div>
                     </div>
 
@@ -44,8 +44,8 @@ class Portfolio extends Component {
                     <div id='main'>
                         <Switch>
                             <Route id='main-home' exact path="/portfolio" component={Home} />
-                            <Route id='main-about' exact path="/about" component={About} />
-                            <Route id='main-projects' exact path="/projects" component={Projects} />
+                            <Route id='main-about' exact path="/portfolio/about" component={About} />
+                            <Route id='main-projects' exact path="/portfolio/projects" component={Projects} />
                         </Switch>
                     </div>
 
