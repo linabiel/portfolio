11ec745ac695ec4a97b08fd99f78b0d5a5735f7a
Lina <31240383+linabiel@users.noreply.github.com>
2021-07-30 18:29:31 +0100

    about bottom padding

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -6,7 +6,6 @@ import Home from '../components/Home';
 import About from '../components/About';
 import Projects from '../components/Projects';
 
-import '../components/css/main.css';
 import '../components/css/home.css';
 import '../components/css/header.css';
 import '../components/css/carousel.css';
